2ca1f8c7221df961f2e3ea03f8475d678498a30c
Minhaz Uddin <minhaz12219990@gmail.com>
2023-06-21 02:13:00 +0600

    Add Minhaz changes code

---
+++ admin.cpp
@@ -0,0 +1,61 @@
+#include "admin.h"
+#include "ui_admin.h"
+
+Admin::Admin(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Admin)
+{
+    ui->setupUi(this);
+    ui->Login->setCurrentIndex(0);
+
+}
+
+Admin::~Admin()
+{
+    delete ui;
+}
+
+
+
+
+
+
+void Admin::on_BackToMenu_clicked()
+{
+    this->hide();
+    MainMenu *main_window;
+    main_window = new MainMenu();
+    main_window->show();
+}
+
+
+void Admin::on_Admin_login_Button_clicked()
+{
+    admin_username = ui->Admin_login_Input_UserName->text();
+    admin_userpass = ui->Admin_login_Input_Password->text();
+
+    if(admin_username == "admin" && admin_userpass == "admin"){
+        ui->Login->setCurrentIndex(1);
+    }
+    else{
+        QMessageBox::information(this,"Login","Invalid UserName or Password!");
+    }
+    ui->Admin_login_Input_Password->clear();
+    ui->Admin_login_Input_UserName->clear();
+}
+
+
+void Admin::on_Menu_Button_clicked()
+{
+        ui->Login->setCurrentIndex(0);
+}
+
+
+void Admin::on_BackToMenu_2_clicked()
+{
+        this->hide();
+        MainMenu *main_window;
+        main_window = new MainMenu();
+        main_window->show();
+}
+
---
+++ mainmenu.h
@@ -0,0 +1,41 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+#include "admin.h"
+#include "user.h"
+#include "deliveryboy.h"
+#include "seller.h"
+#include "warehouse.h"
+#include <QSqlDatabase>
+#include <QSqlQuery>
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainMenu; }
+QT_END_NAMESPACE
+
+class MainMenu : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainMenu(QWidget *parent = nullptr);
+    ~MainMenu();
+
+private slots:
+    void on_pushButton_admin_clicked();
+
+    void on_pushButton_user_clicked();
+
+    void on_pushButton_seller_clicked();
+
+    void on_pushButton_warehouse_clicked();
+
+    void on_pushButton_deliveryBoy_clicked();
+
+private:
+    Ui::MainMenu *ui;
+    QSqlDatabase db3;
+};
+#endif // MAINMENU_H
